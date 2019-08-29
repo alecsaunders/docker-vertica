8dc052f347665edff1b473f1dc430cd1184a9232
Alec Saunders <alecsaunders90@gmail.com>
2019-08-29 09:27:32 -0600

    Edit dockerfile and docker-entrypoint

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -3,53 +3,36 @@ set -e
 
 STOP_LOOP="false"
 
-# if DATABASE_NAME is not provided use default one: "docker"
-export DATABASE_NAME="${DATABASE_NAME:-docker}"
-# if DATABASE_PASSWORD is provided, use it as DB password, otherwise empty password
-if [ -n "$DATABASE_PASSWORD" ]; then export DBPW="-p $DATABASE_PASSWORD" VSQLPW="-w $DATABASE_PASSWORD"; else export DBPW="" VSQLPW=""; fi
-
 # Vertica should be shut down properly
 function shut_down() {
   echo "Shutting Down"
-  vertica_proper_shutdown
-  echo 'Saving configuration'
-  mkdir -p ${VERTICADATA}/config
-  /bin/cp /opt/vertica/config/admintools.conf ${VERTICADATA}/config/admintools.conf
-  echo 'Stopping loop'
   STOP_LOOP="true"
-}
-
-function vertica_proper_shutdown() {
-  echo 'Vertica: Closing active sessions'
-  /bin/su - dbadmin -c "/opt/vertica/bin/vsql -U dbadmin -d ${DATABASE_NAME} ${VSQLPW} -c 'SELECT CLOSE_ALL_SESSIONS();'"
-  echo 'Vertica: Flushing everything on disk'
-  /bin/su - dbadmin -c "/opt/vertica/bin/vsql -U dbadmin -d ${DATABASE_NAME} ${VSQLPW} -c 'SELECT MAKE_AHM_NOW();'"
-  echo 'Vertica: Stopping database'
-  /bin/su - dbadmin -c "/opt/vertica/bin/admintools -t stop_db ${DBPW} -d ${DATABASE_NAME} -i"
-}
-
-function fix_filesystem_permissions() {
-  chown -R dbadmin:verticadba "${VERTICADATA}"
-  chown dbadmin:verticadba /opt/vertica/config/admintools.conf
 }
 
 trap "shut_down" SIGKILL SIGTERM SIGHUP SIGINT
 
+# if DATABASE_NAME is not provided use default one: "docker"
+export DATABASE_NAME="${DATABASE_NAME:-docker}"
 
-echo 'Starting up'
-if [ ! -f ${VERTICADATA}/config/admintools.conf ]; then
-  echo 'Fixing filesystem permissions'
-  fix_filesystem_permissions
-  echo 'Creating database'
-  su - dbadmin -c "/opt/vertica/bin/admintools -t create_db --skip-fs-checks -s localhost -d ${DATABASE_NAME} ${DBPW} -c ${VERTICADATA}/catalog -D ${VERTICADATA}/data"
-else
-  echo 'Restoring configuration'
-  cp ${VERTICADATA}/config/admintools.conf /opt/vertica/config/admintools.conf
-  echo 'Fixing filesystem permissions'
-  fix_filesystem_permissions
-  echo 'Starting Database'
-  su - dbadmin -c "/opt/vertica/bin/admintools -t start_db -d ${DATABASE_NAME} ${DBPW} --noprompts --timeout=never"
+# if DATABASE_PASSWORD is provided, use it as DB password, otherwise empty password
+if [ -n "$DATABASE_PASSWORD" ]; then export DBPW="-p $DATABASE_PASSWORD" VSQLPW="-w $DATABASE_PASSWORD"; else export DBPW="" VSQLPW=""; fi
+
+CREATE_DB="/opt/vertica/bin/admintools -t create_db --skip-fs-checks -s localhost -d ${DATABASE_NAME} ${DBPW}"
+
+if [[ -n $CATALOGPATH ]]; then
+    mkdir -p $CATALOGPATH
+    chown -R dbadmin: $CATALOGPATH
+    CREATE_DB="${CREATE_DB} --catalog_path ${CATALOGPATH}"
 fi
+
+if [[ -n $DATAPATH ]]; then
+    mkdir -p $DATAPATH
+    chown -R dbadmin: $DATAPATH
+    CREATE_DB="${CREATE_DB} --data_path ${DATAPATH}"
+fi
+
+echo 'Creating database'
+su - dbadmin -c "${CREATE_DB}"
 
 echo
 if [ -d /docker-entrypoint-initdb.d/ ]; then
